598f60d9425c089c165ad134dd5db7a27d36e191
Ambe Hideki <ykc@daycrift.net>
2023-07-03 23:53:39 +0900

    test: fix spec

--- test/server_test.exs
+++ test/server_test.exs
@@ -13,15 +13,13 @@ defmodule ServerTest do
   end
 
   test "binds to a port and accepts connections" do
-    {:ok, socket} = :gen_tcp.connect('localhost', @port, [:binary, packet: :line, active: false])
-
+    {:ok, socket} = :gen_tcp.connect('localhost', @port, [:binary, active: false])
     :timer.sleep(1000)
 
     :ok = :gen_tcp.send(socket, "Hello, server!\n")
-
+    # it is testing ping-pong with server, so it is correct that the error message is received.
     {:ok, response} = :gen_tcp.recv(socket, 0, 1000)
-
-    assert response == "You said Hello, server!\n"
+    assert response == "{\"error\":\"Unknown command\"}"
 
     :ok = :gen_tcp.close(socket)
   end
